41b716341a8e3747e64b6c6e001c2102e9ec4f17
EthanForrestCarr <ethan@ethancarr.com>
2024-10-25 18:54:32 -0500

    AboutMe foundation

---
+++ portfolio/src/pages/AboutMe.tsx
@@ -0,0 +1,11 @@
+function AboutMe() {
+    return (
+      <section>
+        <h2>About Me</h2>
+        <img src="{/*image path goes here*/}" alt="Ethan Carr" />
+        <p>{/*bio goes here*/}</p>
+      </section>
+    );
+  }
+  
+  export default AboutMe;  
