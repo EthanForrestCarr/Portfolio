aef7b8be668e23a6b9a3927f6a17b7776a002ee7
EthanForrestCarr <ethan@ethancarr.com>
2024-10-25 18:50:53 -0500

    footer foundation

---
+++ portfolio/src/components/Footer.tsx
@@ -0,0 +1,10 @@
+function Footer() {
+    return (
+      <footer>
+        <a href="https://github.com/EthanForrestCarr" target="_blank" rel="noopener noreferrer">GitHub</a>
+        <a href="https://www.linkedin.com/in/ethan-carr" target="_blank" rel="noopener noreferrer">LinkedIn</a>
+      </footer>
+    );
+  }
+  
+  export default Footer;  
