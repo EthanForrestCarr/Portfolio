01a68952f2dfcf2812b7533c00c232d9df02b67f
EthanForrestCarr <ethan@ethancarr.com>
2024-10-25 19:09:09 -0500

    porfolio foundation

---
+++ portfolio/src/pages/Portfolio.tsx
@@ -0,0 +1,13 @@
+import Project from '../components/Project';
+
+function Portfolio() {
+  return (
+    <section>
+      <h2>Portfolio</h2>
+      <Project title="Project" imageUrl="_blank" deployedUrl="_blank" githubUrl="https://github.com/EthanForrestCarr/" />
+      {/* add projects here */}
+    </section>
+  );
+}
+
+export default Portfolio;
