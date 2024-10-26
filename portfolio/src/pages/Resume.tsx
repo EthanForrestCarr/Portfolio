5ca2c5e95d89969c85d34394ea3a6a3a47b1af07
EthanForrestCarr <ethan@ethancarr.com>
2024-10-25 19:15:33 -0500

    resume foundation

---
+++ portfolio/src/pages/Resume.tsx
@@ -0,0 +1,17 @@
+function Resume() {
+    return (
+      <section>
+        <h2>Resume</h2>
+        <a href="/path/to/resume.pdf" download>Download Resume</a>
+        <h3>Proficiencies</h3>
+        <ul>
+          <li>JavaScript</li>
+          <li>React</li>
+          <li>TypeScript</li>
+          {/* more skills */}
+        </ul>
+      </section>
+    );
+  }
+  
+  export default Resume;
