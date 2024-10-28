519fa6e15e3142e931e362f2fe6747ca5201a6b0
EthanForrestCarr <ethan@ethancarr.com>
2024-10-27 20:48:26 -0500

    added an image route for now... waiting to go over netlify before I finish

--- portfolio/src/pages/AboutMe.tsx
+++ portfolio/src/pages/AboutMe.tsx
@@ -2,10 +2,10 @@ function AboutMe() {
     return (
       <section>
         <h2>About Me</h2>
-        <img src="{/*image path goes here*/}" alt="Ethan Carr" />
-        <p>{/*bio goes here*/}</p>
+        <img src="/images/Chester.jpeg" alt="Ethan Carr" />
+        <p>Ethan Carr is a curious and dynamic learner making his way from the kitchen to the keyboard. With a background in culinary arts and environmental work, Ethan’s journey into tech is as eclectic as his interests. When he’s not writing code, you’ll find him practicing piano, exploring new skateboarding tricks, or reading about sharks. With a recent full-stack certificate under his belt, Ethan combines his varied experiences with a fresh perspective on software engineering—navigating new challenges, learning as he goes, and enjoying the ride.</p>
       </section>
     );
   }
   
-  export default AboutMe;  
+  export default AboutMe;
--- portfolio/src/pages/Portfolio.tsx
+++ portfolio/src/pages/Portfolio.tsx
@@ -1,10 +1,11 @@
 import Project from '../components/Project';
+import oneImage from "../assets/images/EmployeeTracker.png"
 
 function Portfolio() {
   return (
     <section>
       <h2>Portfolio</h2>
-      <Project title="Employee Tracker" imageUrl="/images/EmployeeTracker.png" deployedUrl="_blank" githubUrl="https://github.com/EthanForrestCarr/EmployeeTracker" />
+      <Project title="Employee Tracker" imageUrl={oneImage} deployedUrl="_blank" githubUrl="https://github.com/EthanForrestCarr/EmployeeTracker" />
       <Project title="README.md Generator" imageUrl="/images/ReadMeGenerator.png" deployedUrl="_blank" githubUrl="https://github.com/EthanForrestCarr/ReadMeGenerator" />
       <Project title="Vehicle Builder" imageUrl="/images/VehicleBuilder.png" deployedUrl="_blank" githubUrl="https://github.com/EthanForrestCarr/VehicleBuilder" />
       <Project title="Movie Database" imageUrl="/images/MovieDatabase.png" deployedUrl="_blank" githubUrl="https://github.com/EthanForrestCarr/MovieDatabase" />
