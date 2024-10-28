1c684597dd30d42b45e862c5b0248558d2c48d9c
EthanForrestCarr <ethan@ethancarr.com>
2024-10-27 19:10:20 -0500

    filled out portfolio

--- portfolio/src/pages/Portfolio.tsx
+++ portfolio/src/pages/Portfolio.tsx
@@ -4,8 +4,12 @@ function Portfolio() {
   return (
     <section>
       <h2>Portfolio</h2>
-      <Project title="Project" imageUrl="_blank" deployedUrl="_blank" githubUrl="https://github.com/EthanForrestCarr/" />
-      {/* add projects here */}
+      <Project title="Employee Tracker" imageUrl="/images/EmployeeTracker.png" deployedUrl="_blank" githubUrl="https://github.com/EthanForrestCarr/EmployeeTracker" />
+      <Project title="README.md Generator" imageUrl="/images/ReadMeGenerator.png" deployedUrl="_blank" githubUrl="https://github.com/EthanForrestCarr/ReadMeGenerator" />
+      <Project title="Vehicle Builder" imageUrl="/images/VehicleBuilder.png" deployedUrl="_blank" githubUrl="https://github.com/EthanForrestCarr/VehicleBuilder" />
+      <Project title="Movie Database" imageUrl="/images/MovieDatabase.png" deployedUrl="_blank" githubUrl="https://github.com/EthanForrestCarr/MovieDatabase" />
+      <Project title="Prework Study Guide" imageUrl="/images/PreWorkStudyGuide.png" deployedUrl="_blank" githubUrl="https://github.com/EthanForrestCarr/prework-study-guide" />
+      <Project title="Weather Dashboard" imageUrl="/images/WeatherDashboard.png" deployedUrl="_blank" githubUrl="https://github.com/EthanForrestCarr/WeatherDashboard" />
     </section>
   );
 }
