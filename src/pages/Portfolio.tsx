ae12d50ef66ac4b49846f83a035237f18702d48b
EthanForrestCarr <ethan@ethancarr.com>
2024-10-28 19:32:41 -0500

    added the rest of the images

--- src/pages/Portfolio.tsx
+++ src/pages/Portfolio.tsx
@@ -1,6 +1,10 @@
 import Project from '../components/Project';
 import employeeTracker from "../assets/images/EmployeeTracker.png"
 import readMeGenerator from "../assets/images/ReadMeGenerator.png"
+import vehicleBuilder from "../assets/images/VehicleBuilder.png"
+import movieDatabase from "../assets/images/MovieDatabase.png"
+import preworkStudyGuide from "../assets/images/PreWorkStudyGuide.png"
+import weatherDashboard from "../assets/images/WeatherDashboard.png"
 
 function Portfolio() {
   return (
@@ -8,10 +12,10 @@ function Portfolio() {
       <h2>Portfolio</h2>
       <Project title="Employee Tracker" imageUrl={employeeTracker} deployedUrl="_blank" githubUrl="https://github.com/EthanForrestCarr/EmployeeTracker" />
       <Project title="README.md Generator" imageUrl={readMeGenerator} deployedUrl="_blank" githubUrl="https://github.com/EthanForrestCarr/ReadMeGenerator" />
-      <Project title="Vehicle Builder" imageUrl="/images/VehicleBuilder.png" deployedUrl="_blank" githubUrl="https://github.com/EthanForrestCarr/VehicleBuilder" />
-      <Project title="Movie Database" imageUrl="/images/MovieDatabase.png" deployedUrl="_blank" githubUrl="https://github.com/EthanForrestCarr/MovieDatabase" />
-      <Project title="Prework Study Guide" imageUrl="/images/PreWorkStudyGuide.png" deployedUrl="_blank" githubUrl="https://github.com/EthanForrestCarr/prework-study-guide" />
-      <Project title="Weather Dashboard" imageUrl="/images/WeatherDashboard.png" deployedUrl="_blank" githubUrl="https://github.com/EthanForrestCarr/WeatherDashboard" />
+      <Project title="Vehicle Builder" imageUrl={vehicleBuilder} deployedUrl="_blank" githubUrl="https://github.com/EthanForrestCarr/VehicleBuilder" />
+      <Project title="Movie Database" imageUrl={movieDatabase} deployedUrl="_blank" githubUrl="https://github.com/EthanForrestCarr/MovieDatabase" />
+      <Project title="Prework Study Guide" imageUrl={preworkStudyGuide} deployedUrl="_blank" githubUrl="https://github.com/EthanForrestCarr/prework-study-guide" />
+      <Project title="Weather Dashboard" imageUrl={weatherDashboard} deployedUrl="_blank" githubUrl="https://github.com/EthanForrestCarr/WeatherDashboard" />
     </section>
   );
 }
