c0455a957ad1c0ca991612ed6dc38657a6bbf59a
EthanForrestCarr <ethan@ethancarr.com>
2024-10-28 19:20:26 -0500

    added an image

--- src/pages/Portfolio.tsx
+++ src/pages/Portfolio.tsx
@@ -1,12 +1,13 @@
 import Project from '../components/Project';
-import oneImage from "../assets/images/EmployeeTracker.png"
+import employeeTracker from "../assets/images/EmployeeTracker.png"
+import readMeGenerator from "../assets/images/ReadMeGenerator.png"
 
 function Portfolio() {
   return (
     <section>
       <h2>Portfolio</h2>
-      <Project title="Employee Tracker" imageUrl={oneImage} deployedUrl="_blank" githubUrl="https://github.com/EthanForrestCarr/EmployeeTracker" />
-      <Project title="README.md Generator" imageUrl="/images/ReadMeGenerator.png" deployedUrl="_blank" githubUrl="https://github.com/EthanForrestCarr/ReadMeGenerator" />
+      <Project title="Employee Tracker" imageUrl={employeeTracker} deployedUrl="_blank" githubUrl="https://github.com/EthanForrestCarr/EmployeeTracker" />
+      <Project title="README.md Generator" imageUrl={readMeGenerator} deployedUrl="_blank" githubUrl="https://github.com/EthanForrestCarr/ReadMeGenerator" />
       <Project title="Vehicle Builder" imageUrl="/images/VehicleBuilder.png" deployedUrl="_blank" githubUrl="https://github.com/EthanForrestCarr/VehicleBuilder" />
       <Project title="Movie Database" imageUrl="/images/MovieDatabase.png" deployedUrl="_blank" githubUrl="https://github.com/EthanForrestCarr/MovieDatabase" />
       <Project title="Prework Study Guide" imageUrl="/images/PreWorkStudyGuide.png" deployedUrl="_blank" githubUrl="https://github.com/EthanForrestCarr/prework-study-guide" />
